89387feb82c23fefb243dc146412676e8cca6466
maxkd <maxkd@192.168.2.62>
2019-05-11 15:52:45 -0400

    created start menu, need to break up into methods tho

--- Checkers/src/chec/kers/GraphicsDriver.java
+++ Checkers/src/chec/kers/GraphicsDriver.java
@@ -3,12 +3,66 @@ package chec.kers;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import javafx.*;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class GraphicsDriver extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Stage window;
 
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		window.setTitle("Welcome to Checkers");
+		final double CANVAS_WIDTH = 550;
+		final double CANVAS_HEIGHT = 550;
+		final Image titleScreen = new Image("asset.jpg"); // title screen image
+		final ImageView flashScreen_node = new ImageView();
+		flashScreen_node.setImage(titleScreen); // set the image of the title screen
+		window.getIcons().add(titleScreen); // stage icon
+		Label label = new Label("Please choose one of the following: ");
+		HBox hbox = new HBox();
+		Button button1 = new Button("1 Player");
+		Button button2 = new Button("2 Players");
+		button1.setOnAction(e -> {
+			boolean result = ConfirmBox.display("Verify", "You selected player vs Ai, do you wish to continue?");
+			if (result) {
+				// ai game
+				System.out.println("ai");
+			}
+		});
+		button2.setOnAction(e -> {
+			boolean result = ConfirmBox.display("Verify", "You selected player vs player, do you wish to continue?");
+			if (result) {
+				// 2 players
+				System.out.println("2 players");
+			}
+		});
+		HBox buttons = new HBox();
+		buttons.getChildren().addAll(button1, button2);
+		VBox layout = new VBox();
+		layout.getChildren().addAll(label,buttons);
+		Group root = new Group();
+		root.getChildren().addAll(flashScreen_node, layout);
+		Scene scene = new Scene(root, CANVAS_WIDTH, CANVAS_HEIGHT, Color.BLACK);
+		window.setScene(scene);
+		window.show();
 	}
 
 }
