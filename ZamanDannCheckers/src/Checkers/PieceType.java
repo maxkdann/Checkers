d9a705b050bcc474e675875d93b252ea54e2dbab
Kiyan <Kiyan@192.168.2.17>
2019-05-24 12:07:01 -0400

    Added king movetype

--- ZamanDannCheckers/src/Checkers/PieceType.java
+++ ZamanDannCheckers/src/Checkers/PieceType.java
@@ -3,11 +3,18 @@ package Checkers;
 public enum PieceType {
 	// blue pieces will be positive as their y locations will increase
 	// red pieces will be the oppostie
-	BLUE(1), RED(-1);
+	BLUE(1), RED(-1), KING(1, -1);
 	//move direction will be positive or negative based on the piece type
 	final int moveDir;
-
+	final int moveDir1;
 	PieceType(int moveDir) {
 		this.moveDir = moveDir;
+		this.moveDir1 = 0;
+	}
+	
+	PieceType(int moveDir, int moveDir1) {
+		this.moveDir = moveDir;
+		this.moveDir1 = moveDir1;
+
 	}
 }
